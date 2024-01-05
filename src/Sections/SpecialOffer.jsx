4877f1a80ef476a766b6c1e51e8555c441fcd52b
Verma <jayant.a.verma@capgemini.com>
2024-01-06 00:12:53 +0530

    special offer section almost done

--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import { offer } from '../assets/images'
+import Button from '../Components/Button'
 
 const SpecialOffer = () => {
   return (
-    <div>SpecialOffer</div>
+    <section className='max-container flex justify-wrap items-center max-xl:flex-col-reverse gap-10'>
+        <div className='flex-1 '>
+          <img src={offer} alt=""  width={773} height={687} className='object-contain w-full'/>
+        </div>
+        <div className="flex-1 flex flex-col">
+        <h2 className="font-palanquin text-4xl capitalize lg:max-w-lg font-bold">
+          We Provide you
+          <span className="text-coral-red "> Super</span>
+          <br />
+          <span className="text-coral-red ">Quality</span> Shoes
+        </h2>
+        <p className="mt-4 lg:max-w-lg info-text">
+          Ensuring premium comfort and style, our meticulously crafted footwear
+          is designed to elavate your experience, providing you with unmatched
+          quality, innovation, and a touch of elegance
+        </p>
+        <p className="mt-6 lg:max-w-lg info-text">
+          Our dedication to detail and excellence ensures your satisfaction{" "}
+        </p>
+        <div className="mt-11">
+          <Button label="View Details"></Button>
+        </div>
+      </div>
+    </section>  
   )
 }
 
